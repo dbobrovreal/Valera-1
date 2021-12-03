0579f816d4e72f7687dc0cc4b2dba0aad782c9f4
dbobrovreal <dbobrovreal@mail.ru>
2021-12-03 14:23:47 +0300

    add spec test action

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -1,51 +1,24 @@
-require './lib/action'
-require './lib/valera'
+require_relative '../lib/action/cond'
+require_relative '../lib/action/effect'
+require_relative '../lib/action/action'
+require_relative '../lib/valera_init'
+require 'rspec'
 
 describe Action do
+  let(:cond) { [].push Action::Cond.new parameter: :health, operator: '<', value: 50 }
+  let(:eff) { [].push Action::Effect.new parameter: :health, value: 5 }
+  let(:character) { ValeraInit.new health: 40 }
+  subject { Action.new name: 'test', conds: cond, effects: eff }
+
   describe '#valid?' do
-    let(:action) do
-      Action.new(
-        conds: [
-          { field: :health, operator: :<, value: 50 },
-          { field: :health, operator: :>, value: 40 }
-        ],
-        effects: []
-      )
-    end
-
-    subject { action.valid? valera }
-
-    context 'when all conds are valid' do
-      let(:valera) { Valera.new health: 45 }
-      it { is_expected.to eq true }
-    end
-
-    context 'when there is an invalid conds' do
-      let(:valera) { Valera.new health: 80 }
-      it { is_expected.to eq false }
+    context 'when the conds is valid' do
+      it { expect(subject.valid?(character)).to be_truthy }
     end
   end
 
-  describe '#execute?' do
-    let(:valera) { Valera.new health: 20, mana: 0 }
-    let(:action) do
-      Action.new(
-        effects: [
-          { field: :health, operator: :-, value: 10 },
-          { field: :mana, operator: :+, value: 10 }
-        ],
-        conds: []
-      )
+  describe '#execute' do
+    context 'when health should be increased by 5' do
+      it { expect(subject.execute(character)).to eq([45]) }
     end
-
-    subject { action.execute valera }
-
-    it 'applies the first effect' do
-      expect { subject }.to change { valera.health }.by(-10)
-    end
-    it 'applies the second effect' do
-      expect { subject }.to change { valera.mana }.by(10)
-    end
-    it { is_expected.to eq valera }
   end
 end
